d98d6a0998a504dbe6bb404dfa9539d5bba36bf8
sylvainvant <sylvainvantieghem@gmail.com>
2018-12-29 00:42:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# site coach sportif
+
+
+premier site de la formation de developpeur web sur le theme de coach sportif en langage HTML/CSS/JS
+
+https://sylvainvant.github.io/coach_sportif/
---
+++ View/404.php
@@ -0,0 +1,7 @@
+<div class="col-md-10 col-md-offset-1 ">
+    <h1 class="title  brand-color">Ooops...</h1>
+       	<h2 class="sub-title  black">The page you were looking for does not exist</h2>
+       <a role="button" href="./" class="btn btn-primary button-medium-padding low-margin-top" title="Return to the Homepage">
+    		<span class="button-text-big"> Return to the Homepage</span>
+		</a>
+</div>
